4c4768021d7b4019748947ad922d5ff4b76955c2
Victor Felder <victorfelder@gmail.com>
2014-02-05 17:17:25 +0100

    New version

--- js/mturk.js
+++ js/mturk.js
@@ -2,26 +2,44 @@ $(document).ready(function(){
   var form = '';
 
   if($('#mturk_form').length > 0) {
-    form = $('#mturk_form');
-  } 
-  // else if($('form[name=hitForm]').length > 0) {
-  //   form = $('form[name=hitForm]')[0];
-  // }
 
-  $(form).submit(function(e) {
-    e.preventDefault();
+    $('#mturk_form').submit(function(e) {
+      e.preventDefault();
 
-    alert('default submit prevented');
+      alert('default submit prevented');
 
-    request = $.ajax({
-      url: $(this).attr('action'),
-      type: "POST",
-      data: $(this).serialize()
-    }).done(function() {
-      alert('form posted, redirecting');
-      window.top.location.href = "https://www.mturk.com/mturk/preview?groupId=2KGW3K4F0OHO8JK8ZWBOAL97PHD01E";
+      request = $.ajax({
+        url: $(this).attr('action'),
+        type: "POST",
+        data: $(this).serialize()
+      }).done(function() {
+        alert('form posted, redirecting');
+        window.top.location.href = "https://www.mturk.com/mturk/preview?groupId=2KGW3K4F0OHO8JK8ZWBOAL97PHD01E";
+      });
+
     });
 
-  });
+
+  } else if($('form[name=hitForm]').length > 0) {
+    form = $('form[name=hitForm]')[0];
+
+    $('input[type=image]').click(function(e) {
+      e.preventDefault();
+
+      alert('default click prevented');
+/*
+      request = $.ajax({
+        url: $(form).attr('action'),
+        type: "POST",
+        data: $(form).serialize()
+      }).done(function() {
+        alert('form posted, redirecting');
+        window.top.location.href = "https://www.mturk.com/mturk/preview?groupId=2KGW3K4F0OHO8JK8ZWBOAL97PHD01E";
+      });
+*/
+    });
+
+
+  }
 
 });
