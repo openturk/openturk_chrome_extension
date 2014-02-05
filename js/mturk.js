3198ea2d519a40a3ca0536dd5cc4dcf6327061de
dedcode <difallah@gmail.com>
2014-02-05 15:29:01 +0100

    html

--- js/mturk.js
+++ js/mturk.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
 
   if($('#mturk_form').length > 0) {
     form = $('#mturk_form');
-  } else if($('form[name=hitForm]').length > 0) {
-    form = $('form[name=hitForm]')[0];
-  }
+  } 
+  // else if($('form[name=hitForm]').length > 0) {
+  //   form = $('form[name=hitForm]')[0];
+  // }
 
   $(form).submit(function(e) {
     e.preventDefault();
@@ -18,7 +19,7 @@ $(document).ready(function(){
       data: $(this).serialize()
     }).done(function() {
       alert('form posted, redirecting');
-      window.top.location.href = "https://stackoverflow.com";
+      window.top.location.href = "https://www.mturk.com/mturk/preview?groupId=2KGW3K4F0OHO8JK8ZWBOAL97PHD01E";
     });
 
   });
