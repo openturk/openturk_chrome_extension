d10e81ce6525443b8028509d2c645439ac69e52c
Victor Felder <victorfelder@gmail.com>
2014-02-18 14:02:18 +0100

    typo fix

--- background.js
+++ background.js
@@ -3,13 +3,11 @@ var autoaccept;
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (typeof request.group_id !== undefined) {
-      if (request.group_id !== "undefined" && typeof request.group_id !== "undefined") {
-        group_id = request.group_id;
-        sendResponse({
-          group_id: group_id
-        });
-      }
+    if (typeof request.group_id !== "undefined" && request.group_id !== "undefined") {
+      group_id = request.group_id;
+      sendResponse({
+        group_id: group_id
+      });
     }
 
     if (request.group_id_get) {
@@ -18,10 +16,8 @@ chrome.runtime.onMessage.addListener(
       });
     }
 
-    if (typeof request.autoaccept !== undefined) {
-      if (typeof request.autoaccept !== "undefined") {
-        autoaccept = request.autoaccept;
-      }
+    if (typeof request.autoaccept !== "undefined") {
+      autoaccept = request.autoaccept;
     }
     if (request.autoaccept_get) {
       sendResponse({
