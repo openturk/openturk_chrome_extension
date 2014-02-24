5e058608af3fb11fb4f65f4da3f5b5e06b904a65
Victor Felder <victorfelder@gmail.com>
2014-02-24 16:38:03 +0100

    Balance in localstorage

--- pages/scripts/popup.js
+++ pages/scripts/popup.js
@@ -206,3 +206,21 @@ $(document).ready(function() {
     read: "resetIcon"
   });
 });
+
+
+
+function get_worker_stats(callback) {
+  $.get('https://workersandbox.mturk.com/mturk/dashboard', {}, function(data) {
+    var rewards = $(data).find('.reward');
+    var approval_rate = $(data).filter("table").find("td.metrics-table-first-value:contains('... Approved')").next().next().text();
+    var balance = {
+      approved_hits: $(rewards[0]).html(),
+      bonuses: $(rewards[1]).html(),
+      total_earnings: $(rewards[2]).html(),
+      approval_rate: approval_rate
+    }
+    localStorage.setItem('balance', balance);
+    callback(balance);
+  });
+}
+get_worker_stats(function(){});
