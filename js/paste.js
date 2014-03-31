1b8bb2448fd2ffa83ee8f905f7458efaa6242fef
Victor Felder <victorfelder@gmail.com>
2014-03-31 19:49:28 +0200

    Formatting

--- js/paste.js
+++ js/paste.js
@@ -1,26 +1,28 @@
-$(function(){
+$(function() {
 
     var editable = null;
-    $("input, textarea").mousedown(function(){
+    $("input, textarea").mousedown(function() {
         // Capture the editable element
         editable = $(this);
     });
 
     function getWorkerId() {
         chrome.runtime.sendMessage({
-          get_worker_id: true
+            get_worker_id: true
         }, function(response) {
-            if(editable) {
-               editable.val(editable.val() + response.workerId);
+            if (editable) {
+                editable.val(editable.val() + response.workerId);
             }
         });
-      }
+    }
 
     chrome.extension.onRequest.addListener(
         function(request, sender, sendResponse) {
             if (request.workerId) {
                 getWorkerId();
-                sendResponse({"workerId": true});
+                sendResponse({
+                    "workerId": true
+                });
             }
         }
     );
